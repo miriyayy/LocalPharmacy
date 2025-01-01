0001890ec943b95925378392034c75035ec7369f
senablgn <160766407+senablgn@users.noreply.github.com>
2025-01-01 15:06:16 +0300

    Delete shop/java/src directory

--- shop/java/src/main/java/com/ecommerce/pharmacy/dataAccess/abstracts/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.pharmacy.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.pharmacy.entities.concretes.Category;
-
-public interface CategoryDao extends JpaRepository<Category, Integer>{
-
-}
