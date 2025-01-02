6b11d9caf67981fb3ffd30d2065babf6c409968b
senablgn <160766407+senablgn@users.noreply.github.com>
2025-01-02 19:08:50 +0300

    Delete java-shop/src/main/resources/data.sql

--- java-shop/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO categories (id, category_name) VALUES
-(1, 'Painkiller'),
-(2, 'Vitamins and Supplements'),
-(3, 'Skincare'),
-(4, 'Antibiotics'),
-(5, 'Cold and Flu'),
-(6, 'Baby Care');
