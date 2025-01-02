f1cb486878312ed3afbcf9638ed01eb58f7b2b71
senablgn <160766407+senablgn@users.noreply.github.com>
2025-01-02 15:57:15 +0300

    Add files via upload

---
+++ java-shop/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (id, category_name) VALUES
+(1, 'Painkiller'),
+(2, 'Vitamins and Supplements'),
+(3, 'Skincare'),
+(4, 'Antibiotics'),
+(5, 'Cold and Flu'),
+(6, 'Baby Care');
