a5c58c5dd79602cf643c4545cd1a6e40dde67957
senablgn <160766407+senablgn@users.noreply.github.com>
2025-01-02 14:59:31 +0300

    Delete src directory

--- src/main/java/com/springBoot/pharmacy/dataAccess/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.springBoot.pharmacy.dataAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springBoot.pharmacy.entities.Category;
-
-public interface CategoryDao extends JpaRepository<Category, Integer>{
-
-}
--- src/main/java/com/springBoot/pharmacy/dataAccess/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.springBoot.pharmacy.dataAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springBoot.pharmacy.entities.Product;
-
-public interface ProductDao extends JpaRepository<Product, Integer>{
-
-}
